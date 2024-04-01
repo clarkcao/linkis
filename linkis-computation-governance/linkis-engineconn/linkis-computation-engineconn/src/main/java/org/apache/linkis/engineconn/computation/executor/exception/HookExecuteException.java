172d8c8048b66dc6151fa7cc1469ceb28929d6ef
peacewong <peacewong@apache.org>
2024-04-01 21:46:49 +0800

    修改异常类型和修改日志打印

--- linkis-computation-governance/linkis-engineconn/linkis-computation-engineconn/src/main/java/org/apache/linkis/engineconn/computation/executor/exception/HookExecuteException.java
+++ linkis-computation-governance/linkis-engineconn/linkis-computation-engineconn/src/main/java/org/apache/linkis/engineconn/computation/executor/exception/HookExecuteException.java
@@ -17,9 +17,10 @@
 
 package org.apache.linkis.engineconn.computation.executor.exception;
 
-import org.apache.linkis.common.exception.ErrorException;
+import org.apache.linkis.common.exception.ExceptionLevel;
+import org.apache.linkis.common.exception.LinkisRuntimeException;
 
-public class HookExecuteException extends ErrorException {
+public class HookExecuteException extends LinkisRuntimeException {
 
   public HookExecuteException(int errCode, String desc) {
     super(errCode, desc);
@@ -27,5 +28,10 @@ public class HookExecuteException extends ErrorException {
 
   public HookExecuteException(int errCode, String desc, String ip, int port, String serviceKind) {
     super(errCode, desc, ip, port, serviceKind);
+  }
+
+  @Override
+  public ExceptionLevel getLevel() {
+    return ExceptionLevel.ERROR;
   }
 }
